2b2225f0886bd30bd24e91651f628fa7ff20ae37
sirutell <sirutell@gmail.com>
2022-03-16 21:36:16 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,6 +3,6 @@
 
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 
-print "A change is a coming!\n";
+print "A change is a coming!\n testing application using openshift web console";
 
 ?>
